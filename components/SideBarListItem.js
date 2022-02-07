7a17912992dbe6921060df46be8d598ed6b3baaf
Ibrahim Hassouna <ihassou1@swarthmore.edu>
2022-02-07 18:05:31 -0500

    sideBar

--- components/SideBarListItem.js
+++ components/SideBarListItem.js
@@ -1,10 +1,23 @@
+import styled from "styled-components";
 
 const SideBarListItem = ({department}) => {
     return (
-      <div >
+      <ListItem className="bg-sky-600 hover:bg-sky-700 " >
           {department}
-      </div>
+      </ListItem>
     )
   };
-  
+  const ListItem = styled.div.attrs({
+    className: "bg-gray-200 hover:bg-red-300 ",
+  })`
+    color:black;
+    margin:10px;
+    text-align:center;
+    width:200px;
+    border-color:red;
+    border: 1px solid red; 
+    border-radius:10px;
+    font:Verdana,Tahoma,"DejaVu Sans",sans-serif;
+    font-size: 20px;
+  `
   export default SideBarListItem;
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,15 +1,25 @@
+import styled from "styled-components";
 import SideBarListItem from "./SideBarListItem";
 const SideBar = () => {
     const departments=["chemistry","physics", "math"]
     let list=[]
-    for (let department of departments){
-        list.push(<SideBarListItem department={department}></SideBarListItem>)
+    for (let i=0;i<40;i++){
+        list.push(<SideBarListItem department={departments[0]}></SideBarListItem>)
     }
     return (
-      <div>
+      <>
+      <h1 className=" ml-5 font-bold text-4xl px-3 py-2 text-slate-700 rounded-lg">Departments</h1>
+      <SideBarList>
       {list}
-      </div>
+      </SideBarList>
+      </>
     )
   };
-  
+  const SideBarList=styled.div`
+    width:230px;
+    margin:30px;
+    align:center;
+    overflow-y: scroll;
+    height:580px;
+  `
   export default SideBar;
