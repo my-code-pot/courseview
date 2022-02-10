08347a43517ee1e764a447da5407911345ff9033
Ibrahim Hassouna <ihassou1@swarthmore.edu>
2022-02-10 15:35:40 -0500

    added departmentsCourses to mongoDB

--- pages/api/scrapers.js
+++ pages/api/scrapers.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-
+const { MongoClient }= require('mongodb');
 const fetchDepartments = async () => {
 	try {
 		const response = await axios.get('https://catalog.swarthmore.edu/content.php?catoid=7&navoid=194');
@@ -46,23 +46,38 @@ const fetchCourses = async (url) =>{
 
 const getCoursesForAllDepartments= async () =>{
     const departments=await fetchDepartments();
-    departmentsMap={};
+    departmentsCourses=[];
     for (let i=0;i<departments.length;i++){
         try{
 
             const courses= await fetchCourses(departments[i].url);
 
-            departmentsMap[departments[i].name]=courses;
+            departmentsCourses.push({
+                "name":departments[i].name,
+                "courses":courses,
+            });
 
         } catch{
             throw error;
         }
     }
-    return departmentsMap;
+    return departmentsCourses;
 }
 
-const test= async ()=>{
-    console.log(await getCoursesForAllDepartments());
+async function InsertIntoMongoDB(){
+    const url="mongodb+srv://ihassou1:Swarth_12345@cluster0.jmjdl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    const client= new MongoClient(url);
+    const departmentsCourses=await getCoursesForAllDepartments();
+    try {
+        await client.connect(); 
+        await client.db("CourseView").collection("departmentsCourses").insertMany(departmentsCourses);
+    }
+    catch(e) {
+        console.log(e);
+    }
+    finally {
+        await client.close();
+    }
 }
-test()
+InsertIntoMongoDB().catch(console.error);
 
--- pages/api/tempCodeRunnerFile.js
+++ pages/api/tempCodeRunnerFile.js
@@ -1,14 +1,3 @@
-console.log(Myhtml);
-// const $=cheerio.load(Myhtml)
-// const departments=[]
-// const heading=$('.program-list li').each((e,dep)=>{
-//     const department=$(dep)
-//     departments.push({
-//         name:department.text(),
-//         url:department.find('a').attr('href')
-//     })
-// })
-// for (let department of departments){
-//     console.log(department.url);
-// }
-
+await client.db("CourseView").collection("departmentsCourses").insertOne({
+        //     "name":"physics"
+        // })
