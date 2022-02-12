9afecc829c43b02310d3b747e10f7b54db7ed86f
Ibrahim Hassouna <ihassou1@swarthmore.edu>
2022-02-12 14:42:54 -0500

    testing mongodb

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,24 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const { MongoClient }= require('mongodb');
-async function getCoursesFromDB(department){
-  const url="mongodb+srv://ihassou1:Swarth_12345@cluster0.jmjdl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  const client= new MongoClient(url);
-  try {
-      await client.connect(); 
-      const courses=await client.db("CourseView").collection("departmentsCourses").find({name
-        :"Art and Art History"});
-      return courses.name;
-  }
-  catch(e) {
-      console.log(e);
-  }
-  finally {
-      await client.close();
-  }
-}
-async function main(){
-  console.log(await getCoursesFromDB("Cognitive Science"));
-} 
-// export default function handler(req, res) {
-//   res.status(200).json({ name: 'John Doe' })
-// }
+const mongoose =require('mongoose');
+
+const url="mongodb+srv://ihassou1:Swarth_12345@cluster0.jmjdl.mongodb.net/CourseView?retryWrites=true&w=majority"
+mongoose.connect(url, {useNewUrlParser:true})
+.then(
+  console.log("hello")
+).catch(
+  (err)=> console.log(err)
+);
+ 
+
--- pages/api/tempCodeRunnerFile.js
+++ pages/api/tempCodeRunnerFile.js
@@ -1,3 +1,13 @@
-export default function handler(req, res) {
-//   res.status(200).json({ name: 'John Doe' })
-// }
+const url="mongodb+srv://ihassou1:Swarth_12345@cluster0.jmjdl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const client= new MongoClient(url);
+try {
+    await client.connect(); 
+    const courses=await client.db("CourseView").listCollections();
+    console.log(courses);
+}
+catch(e) {
+    console.log(e);
+}
+finally {
+    await client.close();
+}
