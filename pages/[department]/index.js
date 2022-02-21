020af95a7384e1afbb1702eb581016e83f2daa36
Ibrahim Hassouna <ihassou1@swarthmore.edu>
2022-02-21 14:48:30 -0500

    scraping

--- pages/[department]/index.js
+++ pages/[department]/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import Navbar from '../../components/Navbar';
 import CoursesList from '../../components/CoursesList';
 import SideBar from '../../components/Sidebar';
-function index({courses}){
+function index(props){
     const router = useRouter();
     const { department } = router.query;
   return (
     <div className='relative'>
-      <SideBar/>
+      <SideBar departments={props.departments}/>
       <h1 className='ml-8 font-bold underline decoration-slate-700 decoration-2 underline-offset-2 w-3/4 color:red-800 text-center text-3xl px-2 py-3 text-red-700 rounded-lg'>{department} courses </h1>
-      <CoursesList listOfCourses={courses}></CoursesList>
+      <CoursesList listOfCourses={props.courses}></CoursesList>
     </div>
   )
 };
@@ -19,7 +18,8 @@ export async function getStaticPaths() {
   const departments=await response.json();
   const paths=departments.map(department=>{
     return {
-      params: {department:department}
+      params: {department:department
+      }
     }
   })
   return {
@@ -30,10 +30,13 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context) {
   const department=context.params.department;
+  const res=await fetch('http://localhost:3000/api/getDepts');
+  const departments=await res.json();
+  console.log(department);
   const response=await fetch('http://localhost:3000/api/'+department);
   const courses=await response.json(); 
 return {
-  props: {courses}, // will be passed to the page component as props
+  props: {courses,departments}, // will be passed to the page component as props
 }
 }
 
---
+++ pages/courses/[course].js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const course = () => {
+  return <div>This will be the about page</div>;
+};
+
+export default course;
